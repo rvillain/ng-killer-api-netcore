052ff2ba365a0b9cba32c59067a5de6b50a2e460
Orkad <nicolas.gidon.43@gmail.com>
2018-04-11 18:02:07 +0200

    ForeignKey On Mission

--- ng-killer-api-netcore.Models/Mission.cs
+++ ng-killer-api-netcore.Models/Mission.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace NgKillerApiCore.Models
 {
     public class Mission : IEntity<long>
     {
+        [Key]
         public long Id { get; set; }
         public string Title { get; set; }
         public string Difficulty { get; set; }
+        public long AgentId { get; set; }
+
+        [ForeignKey(nameof(AgentId))]
         public Agent Agent { get; set; }
     }
 }
