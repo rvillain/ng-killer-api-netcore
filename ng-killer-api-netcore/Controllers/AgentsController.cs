ea845c75b03283f5f636ed63a53b48edbfa7d094
Orkad <nicolas.gidon.43@gmail.com>
2018-04-11 14:14:30 +0200

    Ajout AgentsController & Rename GamesController

---
+++ ng-killer-api-netcore/Controllers/AgentsController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NgKillerApiCore.DAL;
+using NgKillerApiCore.Models;
+
+namespace NgKillerApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class AgentsController : Controller
+    {
+        private const string GetAgentRouteName = "GetAgent";
+        private readonly KillerContext _context;
+
+
+        public AgentsController(KillerContext context)
+        {
+            _context = context;
+
+            if (_context.Agents.Any())
+            {
+                _context.Agents.Add(new Agent { Id="1", Name = "Villain" });
+                _context.SaveChanges();
+            }
+        }
+        
+        [HttpGet]
+        public IEnumerable<Agent> GetAll()
+        {
+            return _context.Agents.ToList();
+        }
+
+        [HttpGet("{id}", Name = GetAgentRouteName)]
+        public IActionResult Get(string id)
+        {
+            return new ObjectResult(_context.Agents.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Agent agent)
+        {
+            if (agent == null)
+            {
+                return BadRequest();
+            }
+            // TODO valider les données de l'agent
+            _context.Agents.Add(agent);
+            _context.SaveChanges();
+            return CreatedAtRoute(GetAgentRouteName, new {id = agent.Id}, agent);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody]Agent agent)
+        {
+            if (agent == null || agent.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var agentDb = _context.Agents.FirstOrDefault(t => t.Id == id);
+            if (agentDb == null)
+            {
+                return NotFound();
+            }
+
+            agentDb.Status = agent.Status;
+
+            _context.Agents.Update(agentDb);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
